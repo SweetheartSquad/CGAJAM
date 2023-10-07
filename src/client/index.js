b09b05348d94830dcceeedf559ccd6f8bd75acb7
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2023-10-06 20:57:29 -0400

    remove hack
    previous commit fixed both issues

--- src/client/index.js
+++ src/client/index.js
@@ -267,15 +267,8 @@ function loadProgressHandler(__loader, __resource){
 				resource.data.muted = true;
 				resource.data.autoplay = false;
 				resource.data.preload = 'auto';
-				resource.data.style.position = 'absolute';
-				resource.data.style.top = '0';
-				resource.data.style.right = '0';
-				resource.data.style.width = '1px';
-				resource.data.style.height = '1px';
-				resource.data.style.opacity = '0.001';
 				resource.texture = PIXI.VideoBaseTexture.fromVideo(resource.data);
 				resource.texture.mipmap = false;
-				document.body.appendChild(resource.data);
 			}
 		}
 	}
